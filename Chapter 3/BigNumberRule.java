1ae94994d35e16c27e1eb89dccc515ead60a6c83
biglol <biglol@nate.com>
2020-10-05 00:45:26 +0900

    first commit

---
+++ Chapter 3/BigNumberRule.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+
+public class BigNumberRule {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+
+        // N 개의 수를 공백을 기준으로 구분하여 입력 받기
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        int first = arr[n - 1];
+        int second = arr[n - 2];
+
+        // 가장 큰 수가 더해지는 횟수 계산
+        int cnt = (m / (k + 1)) * k;
+        cnt += m % (k + 1);
+
+        int result = 0;
+        result += cnt * first; // 가장 큰 수 더하기
+        result += (m - cnt) * second; // 두 번째로 큰 수 더하기
+        System.out.println(result);
+
+    }
+}
